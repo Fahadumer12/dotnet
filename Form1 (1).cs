53a1dd95c26b27b61f84b04b624155676286767d
Fahadumer12 <Fahadumer12@github.com>
2019-07-19 15:17:24 +0500

    xyz

---
+++ Form1 (1).cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Database
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-CM233FB\\SQLEXPRESS;Initial Catalog=Info;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "delete from student";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private void insertbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(regText.Text!=""&&nameText.Text!=""&& classText.Text!="")
+                {
+                    con.Open();
+                    SqlCommand cmd = con.CreateCommand();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    //Insert into student values('"+regText.Text+ "','"+classText.Text+ "','"+nameText.Text+"')"
+                    //     cmd.Parameters.Add(new SqlParameter("reg", regText.Text));
+                    cmd.Parameters.AddWithValue("reg", regText.Text);
+                    cmd.Parameters.Add(new SqlParameter("name", nameText.Text));
+                    cmd.Parameters.Add(new SqlParameter("class", classText.Text));
+
+                    cmd.CommandText="enterdata";
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void viewbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "alldata";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                
+            }
+        }
+
+        private void viewcbtn_Click(object sender, EventArgs e)
+        {
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select *from student where class='bscs' AND name='fahad'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void searchbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Select * from student where name = '" +nameText.Text + "'";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        double no1,no2,result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox.Text=textBox.Text+("1");
+        }
+
+        private void buttonequal_Click(object sender, EventArgs e)
+        {
+            no2 = Convert.ToInt16(textBox.Text);
+            result = no1 + no2;
+            textBox.Text = ("");
+            textBox.Text=textBox.Text+(result);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox.Text = textBox.Text + ("2");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox.Text = textBox.Text + ("3");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox.Text = textBox.Text + ("4");
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox.Text = textBox.Text + ("5");
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox.Text = textBox.Text + ("6");
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox.Text = textBox.Text + ("7");
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox.Text = textBox.Text + ("8");
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox.Text = textBox.Text + ("9");
+
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            textBox.Text = textBox.Text + ("0");
+
+        }
+
+        private void buttonplus_Click(object sender, EventArgs e)
+        {
+           
+             no1 = Convert.ToInt16( textBox.Text);
+            textBox.Text =  ("");
+
+        }
+    }
+}
